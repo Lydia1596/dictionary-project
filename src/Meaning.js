1b99830a67b6493cccd085b12e8855eaf2e110c6
Lydia1596 <lydiaprentice@hotmail.co.uk>
2024-01-12 08:59:06 +0000

    Remove example heading

--- src/Meaning.js
+++ src/Meaning.js
@@ -11,7 +11,6 @@ export default function Meaning(props) {
             <p>
               <strong>Definition: </strong> {definition.definition}
               <br />
-              <strong>Example: </strong>
               <em>{definition.example}</em>
             </p>
           </div>
