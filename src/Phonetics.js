d86ea0f305b72764b7e565fc99344bd0fab487b4
Lydia1596 <lydiaprentice@hotmail.co.uk>
2024-01-17 21:33:51 +0000

    Remove <p> from phonetics

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -5,10 +5,8 @@ export default function Phonetics(props) {
   if (props.data.audio && props.data.text) {
     return (
       <div className="phonetics">
-        <p>
-          <ReactAudioPlayer src={props.data.audio} controls />
-          <strong className="text">{props.data.text}</strong>
-        </p>
+        <ReactAudioPlayer src={props.data.audio} controls />
+        <strong className="text">{props.data.text}</strong>
       </div>
     );
   } else {
